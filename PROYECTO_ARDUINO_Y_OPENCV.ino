76c29a7f335bb5e1e24c1e94d6d068382582a141
LUCHO <ibarguen1294@gmail.com>
2018-06-07 19:55:50 -0500

    Add files via upload

---
+++ PROYECTO_ARDUINO_Y_OPENCV.ino
@@ -0,0 +1,48 @@
+#include <Servo.h>
+
+
+
+Servo servo;
+char lectura; //Variable para guardar la lectura del Serial
+ 
+void setup()
+{
+   //Iniciamos el Serial:
+   Serial.begin(19600);
+ 
+   //Inicializamos el pin 3
+   servo.attach(3);
+   pinMode(13,OUTPUT);
+}
+ 
+void loop()
+{
+   //Si recibimos algo por serial, lo guardamos
+   if(Serial.available() >= 1)
+   {
+      lectura = Serial.read();
+ 
+      //-Si recibiste 'h' -> enciende LED
+      //-Si recibiste otro caracter -> apaga el LED
+      //verde
+      if(lectura == 'h')
+      {
+         servo.write(180);
+         digitalWrite(13,HIGH);
+      }
+      
+      //azul
+      if(lectura == 'a')
+      {
+        servo.write(110);
+        digitalWrite(13,HIGH);
+        }
+       //rojo 
+       if(lectura == 'b')
+      {
+        servo.write(30);
+        digitalWrite(13,HIGH);
+        }
+        
+   }
+}
